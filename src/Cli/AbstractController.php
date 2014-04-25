c5faf91159706ce260ba9161ed47f700c3f44c8d
adlawson <adlawson@gmail.com>
2014-04-25 14:28:11 +0100

    Add missing license

--- src/Cli/AbstractController.php
+++ src/Cli/AbstractController.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of PHPTest
+ *
+ * Copyright (c) 2014 Andrew Lawson <http://adlawson.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace PhpTest\Cli;
 
 use Symfony\Component\Console\Command\Command;
